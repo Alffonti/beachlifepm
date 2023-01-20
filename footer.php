1a06d3ff2666d8d8dbf9b796d0fa171c3c336adb
Alf Fonti <alffonti@gmail.com>
2023-01-20 08:02:39 -0500

    content in WP editor

--- footer.php
+++ footer.php
@@ -1,20 +1,3 @@
-<div class="container-fluid footer my-0">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-6 align-items-center">
-      <img class="d-block" src="<?php echo get_stylesheet_directory_uri()?>/assets/images/logo.png" alt="Beach Life Logo" width="180" height="180">
-      <span class="mb-3">© 2023 Beach Life Property Management</span>
-    </div>
-    <div class="col-md-6">
-      <h3 class="mb-4">Contact Us</h3>
-      <p><strong>Location: </strong> <a href="https://goo.gl/maps/aJY7HCMa8Btxru7A9" itemprop="location" target="_blank">PO Box 128, Myrtle Beach, SC 29578</a></p>
-      <p><strong>Phone: </strong> <a href="tel:+18434572900" itemprop="telephone" target="_blank">843-457-2900</a></p>
-      <p><strong>Email: </strong> <a href="mailto:beachlifepm@gmail.com" itemprop="email" target="_blank">beachlifepm@gmail.com</a></p>
-    </div>
-  </footer>
-</div>
-
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-
 <?php wp_footer(); ?>
 </body>
 </html>
